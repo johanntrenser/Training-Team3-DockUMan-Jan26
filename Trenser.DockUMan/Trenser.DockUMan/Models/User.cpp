fd0046a8b2c45ef0fa8c1a0986a03a7cbbaf0333
Akish Babu <akish.babu@trenser.com>
2026-03-31 16:16:39 +0530

    Add Services,Factory

--- Trenser.DockUMan/Trenser.DockUMan/Models/User.cpp
+++ Trenser.DockUMan/Trenser.DockUMan/Models/User.cpp
@@ -1,1 +1,67 @@
 #include "User.h"
+
+User::User()
+{
+	m_id = "";
+	m_name = "";
+	m_password = "";
+	m_email = "";
+	m_phoneNumber = "";
+}
+
+User::User(std::string id, std::string name, std::string password, std::string email, std::string phoneNumber)
+{
+	m_id = id;
+	m_name = name;
+	m_password = password;
+	m_email = email;
+	m_phoneNumber = phoneNumber;
+}
+
+std::string User::getId()
+{
+	return m_id;
+}
+
+std::string User::getName()
+{
+	return m_name;
+}
+
+std::string User::getPassword()
+{
+	return m_password;
+}
+
+std::string User::getEmail()
+{
+	return m_email;
+}
+
+std::string User::getPhoneNumber()
+{
+	return m_phoneNumber;
+}
+
+void User::setId(std::string id)
+{
+	m_id = id;
+}
+
+void User::setName(std::string name)
+{
+	m_name = name;
+}
+
+void User::setPassword(std::string password)
+{
+	m_password = password;
+}
+void User::setEmail(std::string email)
+{
+	m_email = email;
+}
+void User::setPhoneNumber(std::string phoneNumber)
+{
+	m_phoneNumber = phoneNumber;
+}
--- Trenser.DockUMan/Trenser.DockUMan/Models/User.h
+++ Trenser.DockUMan/Trenser.DockUMan/Models/User.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <iostream>
 class User
 {
+private:
+	std::string m_id;
+	std::string m_name;
+	std::string m_password;
+	std::string m_email;
+	std::string m_phoneNumber;
+public:
+	User();
+	User(std::string id, std::string name, std::string password, std::string email, std::string phoneNumber);
+	std::string getId();
+	std::string getName();
+	std::string getPassword();
+	std::string getEmail();
+	std::string getPhoneNumber();
+	/*getRole();
+	getStatus();*/
+	void setId(std::string);
+	void setPassword(std::string);
+	void setName(std::string);
+	void setEmail(std::string);
+	void setPhoneNumber(std::string);
+	/*setRole();
+	setStatus();*/
+
 };
 

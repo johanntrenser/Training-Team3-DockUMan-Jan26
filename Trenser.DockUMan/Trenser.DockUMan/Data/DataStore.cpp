f7121c12d0a102e9972a90102a061218ada04a62
Johann Thomas <johann.thomas@trenser.com>
2026-04-06 16:29:01 +0530

    Update DataStore

--- Trenser.DockUMan/Trenser.DockUMan/Data/DataStore.cpp
+++ Trenser.DockUMan/Trenser.DockUMan/Data/DataStore.cpp
@@ -1,56 +1,61 @@
 #include "DataStore.h"
 
-std::vector<std::shared_ptr<Dock>> DataStore::getDocks() const
+const std::vector<std::shared_ptr<User>>& DataStore::getUsers() const 
+{
+    return m_users;
+}
+
+const std::vector<std::shared_ptr<Dock>>& DataStore::getDocks() const
 {
     return m_docks;
 }
 
-std::vector<std::shared_ptr<Yard>> DataStore::getYard() const
+const std::vector<std::shared_ptr<Yard>>& DataStore::getYard() const
 {
     return m_yard;
 }
 
-std::vector<std::shared_ptr<Container>> DataStore::getContainers() const
+const std::vector<std::shared_ptr<Container>>& DataStore::getContainers() const
 {
     return m_containers;
 }
 
-std::vector<std::shared_ptr<Crane>> DataStore::getCranes() const
+const std::vector<std::shared_ptr<Crane>>& DataStore::getCranes() const
 {
     return m_cranes;
 }
 
-std::vector<std::shared_ptr<Truck>> DataStore::getTrucks() const
+const std::vector<std::shared_ptr<Truck>>& DataStore::getTrucks() const
 {
     return m_trucks;
 }
 
-std::vector<std::shared_ptr<Ship>> DataStore::getShips() const
+const std::vector<std::shared_ptr<Ship>>& DataStore::getShips() const
 {
     return m_ships;
 }
 
-std::vector<std::shared_ptr<Booking>> DataStore::getBookings() const
+const std::vector<std::shared_ptr<Booking>>& DataStore::getBookings() const
 {
     return m_bookings;
 }
 
-std::vector<std::shared_ptr<BillAndPayment>> DataStore::getBillAndPayments() const
+const std::vector<std::shared_ptr<BillAndPayment>>& DataStore::getBillAndPayments() const
 {
     return m_billAndPayments;
 }
 
-std::map<Enums::ContainerType, std::shared_ptr<Threshold>> DataStore::getThresholds() const
+const std::map<Enums::ContainerType, std::shared_ptr<Threshold>>& DataStore::getThresholds() const
 {
     return m_thresholds;
 }
 
-std::vector<std::shared_ptr<Notification>> DataStore::getNotifications() const
+const std::vector<std::shared_ptr<Notification>>& DataStore::getNotifications() const
 {
     return m_notifications;
 }
 
-std::vector<std::shared_ptr<Log>> DataStore::getLogs() const
+const std::vector<std::shared_ptr<Log>>& DataStore::getLogs() const
 {
     return m_logs;
 }
